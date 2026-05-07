2b46c797350385b224c1b5ca3e2af102fbc8fd43
irfanzz-log <syah90009@gmail.com>
2026-05-07 10:34:20 +0700

    add analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, Teko } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,8 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${teko.variable} h-full antialiased`}
-    >
+    > 
+      <Analytics/>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
